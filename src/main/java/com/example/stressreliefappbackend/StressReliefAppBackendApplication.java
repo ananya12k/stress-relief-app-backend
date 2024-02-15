856b4e5b6cce08357ba725cefb02036289101282
Ananya Kulkarni <ananyakulkarni1203@gmail.com>
2024-02-15 09:57:38 +0530

    Initial commit

---
+++ src/main/java/com/example/stressreliefappbackend/StressReliefAppBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.stressreliefappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StressReliefAppBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StressReliefAppBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stressreliefappbackend/StressReliefAppBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stressreliefappbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StressReliefAppBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
